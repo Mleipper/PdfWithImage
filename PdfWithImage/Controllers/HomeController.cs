f305cbed459160f350ced7392d20e55b06b31d76
Michael Leipper <michael.leipper@Gmail.com>
2020-12-01 10:51:13 +0000

    Add project files.

---
+++ PdfWithImage/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfWithImage.Models;
+using PdfWithImage.Report;
+
+namespace PdfWithImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _oHostEnvironment;
+
+        public HomeController(IWebHostEnvironment oHostEnvironment)
+        {
+            _oHostEnvironment = oHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PrintStudent(int param)
+        {
+            List<Student> students = new List<Student>();
+            for (int i = 1; i <= 9; i++)
+            {
+                students.Add(new Student()
+                {
+                    StudentId = i,
+                    Name = "Stu" + i,
+                    Roll = "100" + i
+                });
+            }
+
+            StudentReport rpt = new StudentReport(_oHostEnvironment);
+            return File(rpt.Report(students), "application/pdf");
+        }
+
+    }
+}
---
+++ PdfWithImage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PdfWithImage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
